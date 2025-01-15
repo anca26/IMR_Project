71a7d2952e21d2399800a18c537025129302e983
NAIex <alexneagubiz@gmail.com>
2025-01-15 18:16:12 +0200

    Spider Scene Intensity 3
    AAAAAAAAAAAAAAAAAA

--- Tomb_of_the_Mind/Assets/Project/Scripts/MoveHead.cs
+++ Tomb_of_the_Mind/Assets/Project/Scripts/MoveHead.cs
@@ -1,20 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MoveHead : MonoBehaviour
 {
     [SerializeField]
-    private float sensitivity;
+    private float sensitivity = 1.0f;
+
+    private Vector2 lookInput;
+
+    // Input Action Reference
+    [SerializeField]
+    private InputActionReference lookAction;
+
+    private void OnEnable()
+    {
+        // Enable the input action
+        lookAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Disable the input action
+        lookAction.action.Disable();
+    }
+
     void Update()
     {
-        float rotateHorizontal = Input.GetAxis("Mouse X");
-        float rotateVertical = Input.GetAxis("Mouse Y");
-        
-        transform.Rotate( Vector3.up    * rotateHorizontal * sensitivity); 
-        transform.Rotate(-Vector3.right * rotateVertical   * sensitivity);
-        
+        // Read input
+        lookInput = lookAction.action.ReadValue<Vector2>();
+
+        // Apply rotation
+        transform.Rotate(Vector3.up * lookInput.x * sensitivity);
+        transform.Rotate(Vector3.right * -lookInput.y * sensitivity);
     }
 }
